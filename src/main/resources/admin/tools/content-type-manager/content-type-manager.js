8a936eaf9e0e7e1dde0a482fcf89806e94639795
Persijn Kwekkeboom <pkw@enonic.com>
2021-11-26 17:03:17 +0100

    Adding menu, not working properly #18

--- src/main/resources/admin/tools/content-type-manager/content-type-manager.js
+++ src/main/resources/admin/tools/content-type-manager/content-type-manager.js
@@ -1,14 +1,22 @@
 'use strict'
 
-const Portal = require('/lib/xp/portal')
-const Thymeleaf = require('/lib/thymeleaf')
+const Portal = require('/lib/xp/portal');
+const Thymeleaf = require('/lib/thymeleaf');
+const AdminLib = require('/lib/xp/admin');
 
 exports.get = () => {
-    
-	const view = resolve('content-type-manager.html')
+
+	const view = resolve('content-type-manager.html');
+
+	const assetsUrl = Portal.assetUrl({
+        path: "",
+    });
 
 	const model = {
+		assetsUrl,
 		loadingIcon: Portal.assetUrl({ path: 'img/spinning-loader.gif' }),
+		launcherUrl: AdminLib.getLauncherUrl(),
+		launcherPath: AdminLib.getLauncherPath(),
 		services: {
 			test1: Portal.serviceUrl({ service: 'test1' }),
 			test2: Portal.serviceUrl({ service: 'test2' }),
