cb2ab80c75fad8e7167bf65f724be1d22a070c3a
Bruno Reis <reisfmb@gmail.com>
2021-12-29 15:52:30 -0300

    Started CardContentType component (#141)

---
+++ src/main/resources/admin/tools/vue/components/cards/CardContentType.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+
+    div
+      p Content Type Card
+      RecursiveComponentRender(:path="path")
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Element } from '@reginaldlee/xml-js';
+import * as ModuleContentType from '../../store/ModuleContentType';
+
+// @ts-ignore
+export default Vue.extend({
+  name: 'CardContentType',
+  props: { path: Array },
+  beforeCreate() {
+    ((this.$options || {}).components || {}).RecursiveComponentRender = require('../RecursiveComponentRender.vue').default;
+  },
+  computed: {
+    elements(): Element[] | null {
+      const path = [...this.path as string[], 'elements'];
+      return ModuleContentType.getContentTypeByPath(this.$store)(path) || null;
+    },
+  },
+});
+
+</script>
+
+<style lang="scss" scoped>
+</style>
