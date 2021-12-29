8416090afdd0394e5fe105cbe8ccccbea48ba92b
Bruno Reis <reisfmb@gmail.com>
2021-12-29 15:52:45 -0300

    Started TextInput component (#140)

---
+++ src/main/resources/admin/tools/vue/components/inputs/TextInput.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    div
+        p Text Input - {{ field.label }}
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'TextInput',
+  props: { 
+    field: Object, // label, hint
+    rules: Array,
+    i18n: Boolean,
+    path: Array,
+  },
+});
+</script>
+
+<style lang="scss"></style>
