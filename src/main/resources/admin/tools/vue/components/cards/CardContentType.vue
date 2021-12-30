860950cf2fb3e0622c43894d513689c06cbe86a4
Bruno Reis <reisfmb@gmail.com>
2021-12-30 03:03:23 -0300

    Added fileName handler to CardContentType (#141)

--- src/main/resources/admin/tools/vue/components/cards/CardContentType.vue
+++ src/main/resources/admin/tools/vue/components/cards/CardContentType.vue
@@ -1,31 +1,43 @@
 <template lang="pug">
-
-    div
-      p Content Type Card
-      RecursiveComponentRender(:path="path")
-
+v-card
+  v-card-title {{ fileName }}
+  v-card-text
+    RecursiveComponentRender(:path="path")
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { Element } from '@reginaldlee/xml-js';
-import * as ModuleContentType from '../../store/ModuleContentType';
+import Vue from "vue";
+import { Element } from "@reginaldlee/xml-js";
+import * as ModuleContentType from "../../store/ModuleContentType";
+import * as ModuleFileHandle from "../../store/ModuleFileHandle";
 
-// @ts-ignore
 export default Vue.extend({
-  name: 'CardContentType',
+  name: "CardContentType",
   props: { path: Array },
+  data: () => ({
+    fileName: "",
+  }),
   beforeCreate() {
-    ((this.$options || {}).components || {}).RecursiveComponentRender = require('../RecursiveComponentRender.vue').default;
+    ((this.$options || {}).components || {}).RecursiveComponentRender =
+      require("../RecursiveComponentRender.vue").default;
+  },
+  created() {
+    this.setFileName();
+  },
+  methods: {
+    setFileName() {
+      ModuleFileHandle.getFileName(this.$store).then(
+        (fn) => (this.fileName = fn || "")
+      );
+    },
   },
   computed: {
     elements(): Element[] | null {
-      const path = [...this.path as string[], 'elements'];
+      const path = [...(this.path as string[]), "elements"];
       return ModuleContentType.getContentTypeByPath(this.$store)(path) || null;
     },
   },
 });
-
 </script>
 
 <style lang="scss" scoped>
