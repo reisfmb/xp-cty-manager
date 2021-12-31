dd37943374759113276b9703eddaf3b626b82e27
Bruno Reis <reisfmb@gmail.com>
2021-12-31 15:55:35 -0300

    Improved ModuleContentType (#142)

--- src/main/resources/admin/tools/vue/store/ModuleContentType/index.ts
+++ src/main/resources/admin/tools/vue/store/ModuleContentType/index.ts
@@ -1,9 +1,9 @@
-import * as xml2jsonConverter from '@reginaldlee/xml-js';
-import * as R from 'ramda';
-import { getStoreAccessors } from 'vuex-typescript';
-import { ActionContext } from 'vuex';
-import { IRootState } from '../rootState';
-import { IContentType, IState } from './types';
+import * as xml2jsonConverter from "@reginaldlee/xml-js";
+import * as R from "ramda";
+import { getStoreAccessors } from "vuex-typescript";
+import { ActionContext } from "vuex";
+import { IRootState } from "../rootState";
+import { IContentType, IState, IAllowedValuesToBeAdded } from "./types";
 
 type ModuleContentTypeContext = ActionContext<IState, IRootState>;
 
@@ -12,11 +12,14 @@ const initialStateValues: IState = {
 };
 
 const getters = {
-  getContentTypeByPath: (state: IState) => (path: string[] = []) => {
-    if (!state.contentType) { return null; }
-    return R.view(R.lensPath(path), state.contentType);
-  },
-
+  getContentTypeByPath:
+    (state: IState) =>
+    (path: string[] = []) => {
+      if (!state.contentType) {
+        return null;
+      }
+      return R.view(R.lensPath(path), state.contentType);
+    },
 };
 
 const mutations = {
@@ -26,15 +29,27 @@ const mutations = {
   resetContentType(state: IState) {
     state.contentType = null;
   },
-  setContentTypeByPath(state: IState, data: {path: string[], value: string}) {
-    state.contentType = R.set(R.lensPath(data.path), data.value, state.contentType);
+  setContentTypeByPath(
+    state: IState,
+    data: { path: string[]; value: IAllowedValuesToBeAdded }
+  ) {
+    state.contentType = R.set(
+      R.lensPath(data.path),
+      data.value,
+      state.contentType
+    );
   },
 };
 
 const actions = {
-  setContentTypeFromXmlString(context: ModuleContentTypeContext, contentTypeAsXmlString: string) {
-    const contentType = xml2jsonConverter.xml2js(contentTypeAsXmlString, { compact: false });
-    context.commit('setContentType', contentType);
+  setContentTypeFromXmlString(
+    context: ModuleContentTypeContext,
+    contentTypeAsXmlString: string
+  ) {
+    const contentType = xml2jsonConverter.xml2js(contentTypeAsXmlString, {
+      compact: false,
+    });
+    context.commit("setContentType", contentType);
   },
 };
 
@@ -46,14 +61,24 @@ export const ModuleContentType = {
   actions,
 };
 
-const { read, commit, dispatch } = getStoreAccessors<IState, IRootState>('ModuleContentType');
+const { read, commit, dispatch } = getStoreAccessors<IState, IRootState>(
+  "ModuleContentType"
+);
 
 // Getters ( read )
-export const getContentTypeByPath = read(ModuleContentType.getters.getContentTypeByPath);
+export const getContentTypeByPath = read(
+  ModuleContentType.getters.getContentTypeByPath
+);
 
 // Mutations ( commit )
-export const resetContentType = commit(ModuleContentType.mutations.resetContentType);
-export const setContentTypeByPath = commit(ModuleContentType.mutations.setContentTypeByPath);
+export const resetContentType = commit(
+  ModuleContentType.mutations.resetContentType
+);
+export const setContentTypeByPath = commit(
+  ModuleContentType.mutations.setContentTypeByPath
+);
 
 // Actions ( dispatch )
-export const setContentTypeFromXmlString = dispatch(ModuleContentType.actions.setContentTypeFromXmlString);
+export const setContentTypeFromXmlString = dispatch(
+  ModuleContentType.actions.setContentTypeFromXmlString
+);
--- src/main/resources/admin/tools/vue/store/ModuleContentType/types.ts
+++ src/main/resources/admin/tools/vue/store/ModuleContentType/types.ts
@@ -1,7 +1,11 @@
-import * as xml2jsonConverter from '@reginaldlee/xml-js';
+import * as xml2jsonConverter from "@reginaldlee/xml-js";
 
-export interface IContentType extends xml2jsonConverter.ElementCompact{}
+export interface IContentType extends xml2jsonConverter.ElementCompact {}
 
 export interface IState {
-  contentType: IContentType | null
+  contentType: IContentType | null;
 }
+export type IAllowedValuesToBeAdded =
+  | string
+  | xml2jsonConverter.Element
+  | Array<xml2jsonConverter.Element>;
