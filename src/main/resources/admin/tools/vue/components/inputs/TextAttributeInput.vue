ce1da50a3b8dba7a289265999b65f2fcbd08784e
Bruno Reis <reisfmb@gmail.com>
2022-01-04 10:06:37 -0300

    Created TextAttributeInput component (#147)

---
+++ src/main/resources/admin/tools/vue/components/inputs/TextAttributeInput.vue
@@ -0,0 +1,68 @@
+<template lang="pug">
+.row
+  template(v-for="attribute in attributes")
+    v-col
+      v-text-field(
+        @blur="save",
+        v-model="states[attribute]",
+        :label="capitalize(attribute)",
+        :rules="usedRules",
+        :class="{ required: usesRequiredRule }"
+      )
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import * as ModuleContentType from "../../store/ModuleContentType";
+import rules from "../../util/rules";
+import * as R from "ramda";
+
+export default Vue.extend({
+  name: "TextAttributeInput",
+  props: {
+    path: Array,
+    attributes: Array,
+    rules: Array, // name of the used rules
+  },
+  data: () => ({
+    states: {} as { [key: string]: string },
+    showI18nDialog: false,
+  }),
+  mounted() {
+    const attrsInCty = ModuleContentType.getContentTypeByPath(this.$store)(
+      this.pathToAttributes
+    );
+
+    (this.attributes as string[]).forEach((attr: string) => {
+      Vue.set(this.states, attr, attrsInCty[attr]);
+    });
+  },
+  watch: {},
+  methods: {
+    save() {
+      ModuleContentType.setContentTypeByPath(this.$store, {
+        path: this.pathToAttributes,
+        value: this.states,
+      });
+    },
+    capitalize(s: string): string {
+      return s.charAt(0).toUpperCase() + s.slice(1);
+    },
+  },
+  computed: {
+    pathToAttributes(): string[] {
+      return [...(this.path as string[]), "attributes"];
+    },
+    usedRules(): Function[] {
+      return R.props(this.rules as string[], rules);
+    },
+    usesRequiredRule(): boolean {
+      return (this.rules as string[]).some((ruleName: string) =>
+        ruleName.includes("required")
+      );
+    },
+  },
+});
+</script>
+
+<style lang="scss"></style>
