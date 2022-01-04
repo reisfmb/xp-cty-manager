bca4edadce900b9300f25ff15256828949002a40
Bruno Reis <reisfmb@gmail.com>
2022-01-04 10:12:43 -0300

    Created CardForm component (#149)

---
+++ src/main/resources/admin/tools/vue/components/cards/CardForm.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+div
+  RecursiveComponentRender(:path="path")
+  NewElementButton(:path="path", :hide="['Option']")
+</template>
+
+<script lang="ts">
+// TODO: this.path can be empty if <form> tag wasn't found in the cty xml.
+//       If that is the case, there's the need to add a "form" element to the first "elements",
+//       rendered by CardContentType component.
+import Vue from "vue";
+import RecursiveComponentRender from "../RecursiveComponentRender.vue";
+import NewElementButton from "../buttons/NewElementButton.vue";
+
+export default Vue.extend({
+  name: "CardForm",
+  components: { RecursiveComponentRender, NewElementButton },
+  props: {
+    path: Array,
+  },
+  data: () => ({}),
+  mounted() {},
+  methods: {},
+  computed: {},
+});
+</script>
+
+<style lang="scss"></style>
