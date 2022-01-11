a4b023e815ad13109b19ad4fc1241c2da0fd3b8c
Bruno Reis <reisfmb@gmail.com>
2022-01-11 16:38:50 -0300

    Renamed option set option (#154)

--- src/main/resources/admin/tools/vue/util/rawSchemas/schemas/option.ts
+++ src/main/resources/admin/tools/vue/util/rawSchemas/schemas/option.ts
@@ -2,7 +2,7 @@ import { xml2js } from "@reginaldlee/xml-js";
 
 export default xml2js(
   `
-<option name="myoptionsetoption">
+<option name="optioninmyoptionset">
   <label/>
   <help-text/>
   <occurrences minimum="0" maximum="1"/>
